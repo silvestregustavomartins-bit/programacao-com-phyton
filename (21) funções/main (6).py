59cbf99e66a14c7bdc7081f3c3f27b2db1c911fe
silvestregustavomartins-bit <silvestregustavomartins@gmail.com>
2026-09-17 10:47:44 -0300

    Add files via upload

---
+++ (21) funções/main (6).py
@@ -0,0 +1,10 @@
+def calcular_media():
+    print("calculando media...")
+    
+# nome - parâmetro
+def saudacao(nome):
+    print(f"bem vindo! {nome}")
+    
+calcular_media()
+saudacao("Corcere") # Corcere argumento
+saudacao("Almir")
