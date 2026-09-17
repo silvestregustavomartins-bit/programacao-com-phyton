cf9e683fcd7039970197ef5563c203d3f95ddf3c
silvestregustavomartins-bit <silvestregustavomartins@gmail.com>
2026-09-17 10:45:19 -0300

    Add files via upload

---
+++ (19) estruturas de dados compostas/main (4).py
@@ -0,0 +1,22 @@
+jogos = [
+    ["CS2", "ROBLOX", "RDR2"], #Jogos de PC
+    ["AstroBot", "God of War", "The Last of Us"], #Jogos de PS4
+    ["Halo", "Forza Horizon", "Gears of War"] #Jogos de XBOX
+    ]
+   
+'''print (len (jogos))''' # Quantidade de lista dentro da Lista (3)
+
+print ("|-Jogos por plataforma!-|\n")
+print ("--------------")
+print ("\nJogos de PC: ")
+for jogo in jogos [0]:
+    print (jogo)
+print ("\n--------------")
+print ("\nJogos de PC: ")
+for jogo in jogos [1]:
+    print (jogo)
+print ("\n--------------")
+print ("\nJogos de XBOX: ")
+for jogo in jogos [2]:
+    print (jogo)
+print ("\n--------------")
