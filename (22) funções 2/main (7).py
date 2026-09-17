c90e3ce6bb381aadff4cad33c30a1b2efdfbaef6
silvestregustavomartins-bit <silvestregustavomartins@gmail.com>
2026-09-17 10:48:37 -0300

    Add files via upload

---
+++ (22) funções 2/main (7).py
@@ -0,0 +1,12 @@
+#def apresentar(nome, idade):
+  #  print("Olá", nome, "Sua idade é", idade)
+#def somar(n1, n2):
+ #   return n1 + n2
+    
+#apresentar("RAIDS HIV", 83417)
+
+def media(n1, n2, n3):
+    media_aritimetica = n1 + n2 + n3 / 3
+    return media_aritimetica
+
+print(media(10, 7, 8))
